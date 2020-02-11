7cb76ca0de32336a15ce70fa882361b987fa7efc
Andrew Chon <chonclan@gmail.com>
2020-02-10 22:38:58 -0600

    dark mode toggle

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log('js loaded');
+
+/* Dark mode toggle */
+const toggle = document.getElementById('theme-button');
+const html = document.documentElement;
+
+//Set dark or light mode theme
+const changeTheme = () => {
+  const currentTheme = html.getAttribute('data-theme');
+
+  if (currentTheme === 'light') {
+    html.setAttribute('data-theme', 'dark');
+    toggle.innerHTML = '☼';
+  } else {
+    html.setAttribute('data-theme', 'light');
+    toggle.innerHTML = '☽';
+  }
+};
+
+//Click functionality
+toggle.addEventListener('click', () => {
+  changeTheme();
+});
