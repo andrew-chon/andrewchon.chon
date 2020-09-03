28cc9bbc015fb5103087d7c46989f3a7637dc3c9
Andrew Chon <andrewdchon@gmail.com>
2020-09-02 22:20:44 -0500

    v2

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 /* Dark mode toggle */
 const toggle = document.getElementById('themeButton');
 const html = document.documentElement;
-const tag = document.getElementById('tagStatus');
+const lookingTag = document.getElementById('lookingTag');
+const educationTag = document.getElementsByClassName('educationTag')
+console.log("educationTag", educationTag)
+const experienceTag = document.getElementsByClassName('experienceTag')
+
 
 //Set dark or light mode theme
 const changeTheme = () => {
@@ -10,11 +14,23 @@ const changeTheme = () => {
   if (currentTheme === 'light') {
     html.setAttribute('data-theme', 'dark');
     toggle.innerHTML = '☼';
-    tag.className = 'is-danger mb-12 tag';
+    lookingTag.className = 'is-primary is-light mb-12 tag';
+    for (let i = 0; i < educationTag.length; i++) {
+      educationTag[i].className = 'educationTag tag is-danger is-light ml-5'
+    }
+    for (let i = 0; i < experienceTag.length; i++) {
+      experienceTag[i].className = 'experienceTag tag is-info is-light ml-5'
+    }
   } else {
     html.setAttribute('data-theme', 'light');
     toggle.innerHTML = '☽';
-    tag.className = 'is-danger is-light mb-12 tag';
+    lookingTag.className = 'is-primary mb-12 tag';
+    for (let i = 0; i < educationTag.length; i++) {
+      educationTag[i].className = 'educationTag tag is-danger ml-5'
+    }
+    for (let i = 0; i < experienceTag.length; i++) {
+      experienceTag[i].className = 'experienceTag tag is-info ml-5'
+    }
   }
 };
 
